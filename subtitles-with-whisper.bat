3e1ccf8baf387da7070acab09b6109bb85b84c60
sealldev <83465508+SSSEAL-C@users.noreply.github.com>
2022-10-02 10:15:52 +1100

    added temp subtitle removal output

--- subtitles-with-whisper.bat
+++ subtitles-with-whisper.bat
@@ -1,54 +1,137 @@
-@echo off
-cls
+###############################
+# Editable Variables
+EXT = "mp4"
+WHISPER_MODEL = "base"
+WHISPER_VERBOSE = False
+WHISPER_LANGUAGE = "English"
+OUTPUTS = "txt,srt,vtt"
+###############################
 
-:: ==============================================================================
-:: Editable Values, Verbose Settings are caps sensitive
-set "PYTHON_CMD=py -m whisper --model base.en --task transcribe --language en"
-set EXT=mp4
-set VERBOSE_WHISPER=True
-set VERBOSE_SCRIPT=True
-:: ==============================================================================
+# Clear terminal and set title variables
+import os
 
-echo Powered By...
-echo   ____                                 _____  _       __          __ _      _                         
-echo  / __ \                         /\    ^|_   _^|( )      \ \        / /^| ^|    (_)                        
-echo ^| ^|  ^| ^| _ __    ___  _ __     /  \     ^| ^|  ^|/  ___   \ \  /\  / / ^| ^|__   _  ___  _ __    ___  _ __ 
-echo ^| ^|  ^| ^|^| '_ \  / _ \^| '_ \   / /\ \    ^| ^|     / __^|   \ \/  \/ /  ^| '_ \ ^| ^|/ __^|^| '_ \  / _ \^| '__^|
-echo ^| ^|__^| ^|^| ^|_) ^|^|  __/^| ^| ^| ^| / ____ \  _^| ^|_    \__ \    \  /\  /   ^| ^| ^| ^|^| ^|\__ \^| ^|_) ^|^|  __/^| ^|   
-echo  \____/ ^| .__/  \___^|^|_^| ^|_^|/_/    \_\^|_____^|   ^|___/     \/  \/    ^|_^| ^|_^|^|_^|^|___/^| .__/  \___^|^|_^|   
-echo         ^| ^|                                                                        ^| ^|                
-echo         ^|_^|                                                                        ^|_^|  
-echo.
-echo Script by Seall.DEV 
-FOR /F %%i IN ('cd') DO set ADDRESS=%%~nxi
-set CWD=%~dp0
-title Subtitling %EXT%'s in %ADDRESS%
-echo.
-echo.
-if %VERBOSE_SCRIPT%==True (
-echo PYTHON_CMD set to "%PYTHON_CMD%"
-echo VERBOSE_WHISPER set to "%VERBOSE_WHISPER%"
+os.system("cls")
+cwd = os.getcwd()
+folder = os.path.basename(cwd)
+os.system(
+    f"title Subtitling {EXT}'s in {folder} ({cwd}) ^- OpenAI's Whisper ^& Seall^.DEV"
 )
-echo Searching for files with extension %EXT%...             
-set /a FILECOUNT = 0
-set /a COUNT = 1
-echo Counting files...
-for %%f in (*.%EXT%) do set /a FILECOUNT += 1
-echo Found %FILECOUNT% files with extension %EXT%!
-IF %FILECOUNT%==0 (
-pause
-exit
+print(
+    """
+  ____                                 _____  _       __          __ _      _                         
+ / __ \                         /\    |_   _|( )      \ \        / /| |    (_)                        
+| |  | | _ __    ___  _ __     /  \     | |  |/  ___   \ \  /\  / / | |__   _  ___  _ __    ___  _ __ 
+| |  | || '_ \  / _ \| '_ \   / /\ \    | |     / __|   \ \/  \/ /  | '_ \ | |/ __|| '_ \  / _ \| '__|
+| |__| || |_) ||  __/| | | | / ____ \  _| |_    \__ \    \  /\  /   | | | || |\__ \| |_) ||  __/| |   
+ \____/ | .__/  \___||_| |_|/_/    \_\|_____|   |___/     \/  \/    |_| |_||_||___/| .__/  \___||_|   
+        | |                                                                        | |                
+        |_|                                                                        |_|  
+Script by Seall.DEV
+
+
+Loading libraries..."""
 )
-echo Subtitling %FILimage.pngECOUNT% files...
-for %%f in (*.%EXT%) do (
-IF EXIST "%%f.srt" (
-title Skipping %%f ^(%COUNT%/%FILECOUNT%^) ^- Subtitling %EXT%'s in %ADDRESS% ^(%CWD:~0,-1%^) ^- OpenAI's Whisper ^& Seall^.DEV
-echo Subtitles already made for "%%f"...
-set /a COUNT += 1
-) ELSE (
-title Working on %%f ^(%COUNT%/%FILECOUNT%^) ^- Subtitling %EXT%'s in %ADDRESS% ^(%CWD:~0,-1%^) ^- OpenAI's Whisper ^& Seall^.DEV
-%PYTHON_CMD% --verbose %VERBOSE_WHISPER% "%%f"
-echo Subtitles for "%%f" completed!
-set /a COUNT += 1
-)
-)
+# Load libraries
+import whisper
+import webvtt
+import ast
+from datetime import timedelta
+
+# Define variable
+filecount = 0
+progcount = 1
+files = []
+START = "start"
+END = "end"
+TEXT = "text"
+print(f"Loading model {WHISPER_MODEL}...")
+# Load model
+model = whisper.load_model(WHISPER_MODEL)
+print(f"Searching for files with the extension {EXT}...")
+for f in os.listdir(cwd):
+    if f.endswith(EXT):
+        filecount += 1
+        files.append(f)
+print(f"Found {str(filecount)} files with extension {EXT}!")
+if filecount == 0:
+    quit
+print(f"Subtitling {str(filecount)} files...")
+def nosubs(progcount):
+    print(f"Subtitles for {f} already exist!")
+    os.system(
+        f"title Skipping {f} ({str(progcount)}/{str(filecount)}) ^- Subtitling {EXT}'s in {folder} ({cwd}) ^- OpenAI's Whisper ^& Seall^.DEV"
+    )
+    progcount += 1
+for f in files:
+    # If subtitles already exist
+    if os.path.exists(f"{f}.srt") or os.path.exists(f"{f}.txt") or os.path.exists(f"{f}.vtt"):
+        if "srt" in OUTPUTS:
+            if os.path.exists(f"{f}.srt"):
+                nosubs(progcount)
+        elif "vtt" in OUTPUTS:
+            if os.path.exists(f"{f}.vtt"):
+                nosubs(progcount)
+        elif "txt" in OUTPUTS:
+            if os.path.exists(f"{f}.txt"):
+                nosubs(progcount)
+        progcount += 1
+    else: 
+        # Generate subtitles
+        print(f"Subtitling {f}...")
+        os.system(
+            f"title Working on {f} ({str(progcount)}/{str(filecount)}) ^- Subtitling {EXT}'s in {folder} ({cwd}) ^- OpenAI's Whisper ^& Seall^.DEV"
+        )
+        if not os.path.exists(f"{f}.temp"):
+            r = model.transcribe(f,language=WHISPER_LANGUAGE,verbose=WHISPER_VERBOSE)
+            with open(f"{f}.temp", "w", encoding="utf-8") as file:
+                file.write(str(r))
+                file.close()
+            print(f"Subtitle data temporarily written to {f}.temp incase of failure.")
+        else:
+            with open(f"{f}.temp", "r", encoding="utf-8") as file:
+                r = ast.literal_eval(file.read())
+                file.close()
+            print(f"Subtitle data read from {f}.temp!")
+        ### TXT ###
+        formatted_outputs = OUTPUTS.split(",")
+        if len(formatted_outputs) == 1:
+            formatted_outputs = "".join(formatted_outputs)
+        else:
+            formatted_outputs[len(formatted_outputs) - 1] = "and " + formatted_outputs[len(formatted_outputs) - 1]
+            formatted_outputs = ", ".join(formatted_outputs)
+        print(f"Making subtitle files to {formatted_outputs}...")
+        if "txt" in OUTPUTS:
+            with open(f"{f}.txt", "w", encoding="utf-8") as file:
+                for seg in r["segments"]:
+                    file.write(f"{str(seg[TEXT])[1:len(str(seg[TEXT]))]}\n")
+                file.close()
+                print(f"Text subtitles written to {f}.txt!")
+        ### SRT ###
+        if "srt" in OUTPUTS or "vtt" in OUTPUTS:
+            with open(f"{f}.srt", "w", encoding="utf-8") as file:
+                for seg in r["segments"]:
+                    id = int(seg["id"]) + 1
+                    seg[START] = str("{:.3f}".format(float(seg[START])))
+                    start = timedelta(seconds=int(str(seg[START]).split(".")[0]))
+                    startdec = str(seg[START]).split(".")[1]
+                    seg[END] = str("{:.3f}".format(float(seg[END])))
+                    end = timedelta(seconds=int(str(seg[END]).split(".")[0]))
+                    enddec = str(seg[END]).split(".")[1]
+                    text = str(seg[TEXT])[1 : len(str(seg[TEXT]))]
+                    file.write(
+                        f"{str(id)}\n{str(start)},{str(startdec)} --> {str(end)},{str(enddec)}\n{text}\n\n"
+                    )
+                file.close()
+                if "srt" in OUTPUTS:
+                    print(f"Text subtitles written to {f}.srt!")
+        ### WEBVTT ###
+        if "vtt" in OUTPUTS:
+            webvtt.from_srt(f"{f}.srt").save(output=f"{f}.vtt")
+            if not "srt" in OUTPUTS:
+                os.remove(f"{f}.srt")
+            print(f"Text subtitles written to {f}.vtt!")
+        ### FINISH ###
+        os.remove(f"{f}.temp")
+        print(f"{f}.temp removed.")
+        print(f"Subtitles finished for {f}!")
+        progcount += 1
